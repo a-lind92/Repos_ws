3b23daf88b63153fd1e1d38bf828fc3b83e0e98d
Arvid Andersson Lind <arvand03@elevad.umea.se>
2025-06-04 22:34:59 +0200

    Ändra saker i guess

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,20 @@
 import adapter from '@sveltejs/adapter-static';
-import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 
-/** @type {import('@sveltejs/kit').Config} */
-const config = {
-
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: [vitePreprocess(), ],
-	extensions: ['.svelte'],
-
-	kit: {
-		adapter: adapter({
-			pages: 'docs',
-			assets: 'docs',
-			fallback: null
-		}),
-		
-	},
+export default {
+  kit: {
+    adapter: adapter({
+      pages: 'docs',    // Output → /docs
+      assets: 'docs',
+      fallback: null
+    }),
+    paths: {
+      base: '/Repos_ws' // Ändra till ditt repo-namn om det inte är root-sida
+    }
+  }
 };
 
-export default config;
+
+
 			
 			
